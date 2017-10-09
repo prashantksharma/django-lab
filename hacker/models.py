3207bebfa75dd574e627b4e7d5d1f50b933afd6f
Prashant Sharma <prashaantsharmaa@users.noreply.github.com>
2017-10-09 23:53:54 +0530

    updated model.py

--- hacker/models.py
+++ hacker/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-	aurthor = models.ForeignKey('auth.User')
+	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=2000)
 	text = models.TextField()
 
